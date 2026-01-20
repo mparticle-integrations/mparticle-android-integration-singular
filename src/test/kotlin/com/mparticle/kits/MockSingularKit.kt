c9adb7bf5a433865a02ca4bcb2f55d8463e47f11
Mansi Pandya <mpandya@mparticle.com>
2026-01-20 13:47:30 -0500

    fix Lint issue

--- src/test/kotlin/com/mparticle/kits/MockSingularKit.kt
+++ src/test/kotlin/com/mparticle/kits/MockSingularKit.kt
@@ -7,35 +7,33 @@ import org.json.JSONException
 import org.json.JSONObject
 
 class MockSingularKit : SingularKit() {
-    override fun logEvent(commerceEvent: CommerceEvent): List<ReportingMessage> {
-        return mutableListOf(
+    override fun logEvent(commerceEvent: CommerceEvent): List<ReportingMessage> =
+        mutableListOf(
             ReportingMessage(
                 this,
                 commerceEvent.eventName,
                 System.currentTimeMillis(),
-                HashMap<String, String>()
-            )
+                HashMap<String, String>(),
+            ),
         )
-    }
 
-    override fun logEvent(mpEvent: MPEvent): List<ReportingMessage> {
-        return mutableListOf(
+    override fun logEvent(mpEvent: MPEvent): List<ReportingMessage> =
+        mutableListOf(
             ReportingMessage(
                 this,
                 mpEvent.eventType.toString(),
                 System.currentTimeMillis(),
-                HashMap<String, String>()
-            )
+                HashMap<String, String>(),
+            ),
         )
-    }
 
     override fun getConfiguration(): KitConfiguration? {
         try {
             return KitConfiguration.createKitConfiguration(
                 JSONObject().put(
                     "id",
-                    MParticle.ServiceProviders.SINGULAR
-                )
+                    MParticle.ServiceProviders.SINGULAR,
+                ),
             )
         } catch (e: JSONException) {
             e.printStackTrace()
